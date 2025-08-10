ee4e108fbf9fa4c031dd75ec455cec060cac887d
e-roy <e-roy@users.noreply.github.com>
2025-08-10 15:15:13 -0400

    chore: ESLint config fixes; add example app configs; scope publish lint; add lint:all

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,35 +1,3 @@
-module.exports = {
-  root: true,
-  extends: [
-    "eslint:recommended",
-    "plugin:@typescript-eslint/recommended"
-  ],
-  parser: "@typescript-eslint/parser",
-  plugins: ["@typescript-eslint"],
-  env: {
-    node: true,
-    es2022: true,
-    jest: true,
-  },
-  parserOptions: {
-    ecmaVersion: 2022,
-    sourceType: "module",
-  },
-  rules: {
-    // TypeScript specific rules
-    "@typescript-eslint/no-unused-vars": ["error", { argsIgnorePattern: "^_" }],
-    "@typescript-eslint/explicit-function-return-type": "off",
-    "@typescript-eslint/explicit-module-boundary-types": "off",
-    "@typescript-eslint/no-explicit-any": "warn",
-    "@typescript-eslint/no-var-requires": "error",
-    "@typescript-eslint/no-empty-function": "warn",
-    "@typescript-eslint/no-inferrable-types": "warn",
-    
-    // General rules
-    "prefer-const": "error",
-    "no-var": "error",
-    "no-console": "warn",
-    "no-debugger": "error",
-  },
-  ignorePatterns: ["dist/", "node_modules/", ".next/", "coverage/", "*.js"],
-};
+import { createRequire } from 'module';
+const require = createRequire(import.meta.url);
+export default require('./.eslintrc.cjs');
